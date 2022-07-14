cfdc7cb379228b00c5ad14426cd8234b8bf6f689
Abnatal <abnatal@gmail.com>
2022-07-14 10:46:44 -0300

    Added SNS

--- lambda_function.py
+++ lambda_function.py
@@ -1,3 +1,4 @@
+import os
 import json
 import boto3
 from sintegra_data import extract_data
@@ -5,10 +6,9 @@ from sintegra_data import extract_data
 def lambda_handler(event, context):
 
     company_data = extract_data(event['html'])
-    
-    sns_client = boto3.client('sns')
-    sns_arn = 'arn:aws:sns:sa-east-1:**********:SintegraNotificationTopic'
-    sns_client.publish(TopicArn = sns_arn, Message = json.dumps(company_data), Subject='Data from Sintegra')
+
+    sns_arn = os.environ['SINTEGRA_SNS_TOPIC_ARN']
+    boto3.client('sns').publish(TopicArn = sns_arn, Message = json.dumps(company_data), Subject='Data from Sintegra')
 
     return {
         'statusCode': 200,
