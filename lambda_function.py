35cc4e313b9f243ecd6ccfd71d6f18d83b184a4a
Abnatal <abnatal@gmail.com>
2022-07-14 15:00:15 -0300

    added API Gateway support

--- lambda_function.py
+++ lambda_function.py
@@ -1,16 +1,18 @@
 import os
 import json
 import boto3
+from datetime import datetime
 from sintegra_data import extract_data
 
 def lambda_handler(event, context):
-
-    company_data = extract_data(event['html'])
+    print(event)
+    company_data = extract_data(json.loads(event['body'])['html'])
 
     sns_arn = os.environ['SINTEGRA_SNS_TOPIC_ARN']
-    boto3.client('sns').publish(TopicArn = sns_arn, Message = json.dumps(company_data), Subject='Data from Sintegra')
+    boto3.client('sns').publish(TopicArn = sns_arn, Message = json.dumps(company_data), Subject='Data from Sintegra [{}]'.format(datetime.now()))
 
     return {
         'statusCode': 200,
+        'headers': { 'Content-Type': 'application/json' },
         'body': json.dumps(company_data)
     }
